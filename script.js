0d49087cd59a4743177e936f723b09ce49f9efb5
Ivan Milenkovic <argentinaivan@hotmail.com>
2021-01-23 04:23:27 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("hi");
+const input = document.querySelector("#number");
+const output = document.querySelector("#output");
+
+const convertToRoman = () => {
+  console.log(input.value);
+  output.innerHTML = `${input.value}`;
+};
+
+input.addEventListener("keyup", convertToRoman);
