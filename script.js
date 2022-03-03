210b5b5f278c86b7053a9f8bd6fcbe54374815bf
Ivan Milenkovic <argentinaivan@hotmail.com>
2022-03-03 21:00:49 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const input = document.querySelector("#number");
 const output = document.querySelector("#output");
 
+const romanInput = document.querySelector("#romanNumber");
+const numberOutput = document.querySelector("#numberOutput");
+
 const convertToRoman = (number) => {
   let romanNumbers = {
     M: 1000,
@@ -29,6 +32,47 @@ const convertToRoman = (number) => {
   return roman;
 };
 
+const convertFromRoman = (text) => {
+  let romanNumbers = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let number = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (
+      (text[i] == "I" && text[i + 1] == "V") ||
+      (text[i] == "I" && text[i + 1] == "X") ||
+      (text[i] == "X" && text[i + 1] == "L") ||
+      (text[i] == "X" && text[i + 1] == "C") ||
+      (text[i] == "C" && text[i + 1] == "D") ||
+      (text[i] == "C" && text[i + 1] == "M")
+    ) {
+      number += romanNumbers[text[i + 1]] - romanNumbers[text[i]];
+      i++;
+    } else {
+      number += romanNumbers[text[i]];
+    }
+  }
+  if (number !== number) return `Not a valid Roman numeral`;
+  return number;
+};
+
 input.addEventListener("keyup", function () {
   output.innerHTML = convertToRoman(input.value);
+});
+
+romanInput.addEventListener("keyup", function () {
+  numberOutput.innerHTML = convertFromRoman(romanInput.value);
 });
